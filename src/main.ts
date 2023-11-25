063515502abf6c26fb60f4155abd4e351caa736d
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-11-25 14:46:36 +0100

    remove cors from nest

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: { origin: true, credentials: true },
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
   const config = new DocumentBuilder()
