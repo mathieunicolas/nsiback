df06a84586a27b2121e087b9bafff3fa7b6e2de9
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-11-27 00:28:18 +0100

    test jwt extended

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,6 +15,10 @@ export class AuthService {
     const payload = {
       username: tmp['cas-user'],
       sub: tmp['cas-attributes']['cas-userAttributes']['id'],
+      firstName: tmp['cas-attributes']['cas-userAttributes']['firstName'],
+      isTeacher: tmp['cas-attributes']['cas-userAttributes']['isTeacher'],
+      classes: tmp['cas-attributes']['cas-userAttributes']['classes'],
+      bday: tmp['cas-attributes']['cas-userAttributes']['birthDate'],
     };
     return {
       access_token: this.jwtService.sign(payload),
